3a2e42257a5a34b1f51ed7c60db88ae0b51842f3
Rishab Singh <rishabsiingh@outlook.com>
2025-05-03 09:39:52 +0530

    add images , improve sidebar ui & ux , add close logic when touch outside sidebar

--- navbar/src/script.js
+++ navbar/src/script.js
@@ -2,16 +2,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const menuBtn = document.querySelector('.mobile-right');
     const sidebar = document.querySelector('.sidebar');
     const closeBtn = document.querySelector('.close-btn')
+    const touchBlock = document.querySelector('.touch-block');
 
     menuBtn.addEventListener('click', ()=> {
         menuBtn.classList.toggle('hidden');
         sidebar.classList.toggle('hidden');
+        touchBlock.classList.toggle('hidden');
     });
 
     closeBtn.addEventListener('click', () => {
         menuBtn.classList.toggle('hidden');
         sidebar.classList.toggle('hidden');
+        touchBlock.classList.toggle('hidden');
     });
+
+    touchBlock.addEventListener('click', ()=> {
+        menuBtn.classList.toggle('hidden');
+        sidebar.classList.toggle('hidden');
+        touchBlock.classList.toggle('hidden');
+    }); 
 
 
 });
