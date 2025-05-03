72ead7ab90af7714830672509238b5fa6a601980
Rishab Singh <rishabsiingh@outlook.com>
2025-05-03 19:00:06 +0530

    add image stop on hovered via js

--- card_with_img_changing_animation/src/index.js
+++ card_with_img_changing_animation/src/index.js
@@ -1,11 +1,24 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     const projectImg = document.querySelector('.project-img');
+    const card = document.querySelector('.card');
 
     const images = ['./images/bg2.jpg', './images/bg3.jpg', './images/bg1.jpg'];
 
     let index = 0;
+    let isHovered = false;
+ 
+    card.addEventListener('mouseenter', () => {
+        isHovered = true;
+    });
+    card.addEventListener('mouseleave', () => {
+        isHovered = false;
+    });
+    
 
 setInterval(()=>{
+
+    if (isHovered) return;
+    
     projectImg.classList.remove('scale-100');
     projectImg.classList.add('scale-90');
       
